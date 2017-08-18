62f47c453a8ebcc2891b927a8f31012f99bad697
cnbs5 <879700945@qq.com>
2017-08-18 14:34:15 +0800

    自定义侧滑界面

--- screens/home.js
+++ screens/home.js
@@ -22,7 +22,6 @@ class HomeScreen extends Component {
         ),
     };*/
 
-
     render() {
         return (
             <View>
@@ -105,7 +104,7 @@ const HomeConst = DrawerNavigator(
         家庭: {
             screen: ContentScreen,
             navigationOptions: ({navigation}) => ({
-                title: '健康',
+                title: '家庭',
                 id: '41'
             }),
         },
@@ -122,16 +121,101 @@ const HomeConst = DrawerNavigator(
         drawerPosition: 'left',
         contentComponent: props =>
             <ScrollView style={styles.bg}>
-                
+
                 <View>
                     <View style={styles.title}>
                         <Image style={styles.cat} source={(require("../assets/images/cat.jpg"))}/>
                         <Text style={styles.text}>请登录</Text>
                     </View>
-
                 </View>
+
+                {/*中间的内容*/}
+                <View style={styles.centerItem}>
+                    <View style={styles.centerItems}>
+                        <Image style={styles.itemsIcon} source={(require("../assets/images/collect.png"))}/>
+                        <Text style={styles.itemsText}>收藏</Text>
+                    </View>
+
+                    <View style={styles.centerItems}>
+                        <Image style={styles.itemsIcon} source={(require("../assets/images/alarm.png"))}/>
+                        <Text style={styles.itemsText}>消息</Text>
+                    </View>
+
+                    <View style={styles.centerItems}>
+                        <Image style={styles.itemsIcon} source={(require("../assets/images/setting.png"))}/>
+                        <Text style={styles.itemsText}>设置</Text>
+                    </View>
+                </View>
+
+                {/*下面的内容列表*/}
+                <View style={styles.contentItem}>
+                    {/*<Image style={styles.contentIcon} source={(require("../assets/images/home.png"))}/>*/}
+                    <Text style={styles.contentText}>首页</Text>
+                    <View style={styles.arrowParent}>
+                        <Image style={styles.contentArrow} source={(require("../assets/images/arrow_right.png"))}/>
+                    </View>
+                </View>
+
+                <View style={styles.contentItem2}>
+                    <Text style={styles.contentText}>情感</Text>
+                    <View style={styles.arrowParent}>
+                        <Image style={styles.contentArrow} source={(require("../assets/images/arrow_right.png"))}/>
+                    </View>
+                </View>
+
+                <View style={styles.contentItem2}>
+                    <Text style={styles.contentText}>人际</Text>
+                    <View style={styles.arrowParent}>
+                        <Image style={styles.contentArrow} source={(require("../assets/images/arrow_right.png"))}/>
+                    </View>
+                </View>
+
+                <View style={styles.contentItem2}>
+                    <Text style={styles.contentText}>成长</Text>
+                    <View style={styles.arrowParent}>
+                        <Image style={styles.contentArrow} source={(require("../assets/images/arrow_right.png"))}/>
+                    </View>
+                </View>
+
+                <View style={styles.contentItem2}>
+                    <Text style={styles.contentText}>学业</Text>
+                    <View style={styles.arrowParent}>
+                        <Image style={styles.contentArrow} source={(require("../assets/images/arrow_right.png"))}/>
+                    </View>
+                </View>
+
+                <View style={styles.contentItem2}>
+                    <Text style={styles.contentText}>职场</Text>
+                    <View style={styles.arrowParent}>
+                        <Image style={styles.contentArrow} source={(require("../assets/images/arrow_right.png"))}/>
+                    </View>
+                </View>
+
+                <View style={styles.contentItem2}>
+                    <Text style={styles.contentText}>健康</Text>
+                    <View style={styles.arrowParent}>
+                        <Image style={styles.contentArrow} source={(require("../assets/images/arrow_right.png"))}/>
+                    </View>
+                </View>
+
+                <View style={styles.contentItem2}>
+                    <Text style={styles.contentText}>家庭</Text>
+                    <View style={styles.arrowParent}>
+                        <Image style={styles.contentArrow} source={(require("../assets/images/arrow_right.png"))}/>
+                    </View>
+                </View>
+
+                <View style={styles.contentItem2}>
+                    <Text style={styles.contentText}>其他</Text>
+                    <View style={styles.arrowParent}>
+                        <Image style={styles.contentArrow} source={(require("../assets/images/arrow_right.png"))}/>
+                    </View>
+                </View>
+
+                {/*</View>*/}
             </ScrollView>
-    });
+    })
+;
 
 const styles = StyleSheet.create({
     bg: {
@@ -153,7 +237,62 @@ const styles = StyleSheet.create({
         fontSize: 16,
         textAlign: 'left',
         marginLeft: 16
+    },
+    centerItem: {
+        flexDirection: 'row',
+        marginTop: 12,
+        marginRight: 24
+    },
+    centerItems: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    itemsIcon: {
+        width: 24,
+        height: 24
+    },
+    itemsText: {
+        color: '#91969d',
+        fontSize: 14,
+        marginTop: 4
+    },
+    contentItem: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingTop: 12,
+        paddingBottom: 12,
+        paddingLeft: 24,
+        paddingRight: 24,
+        marginTop: 12
+    },
+    contentItem2: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingTop: 12,
+        paddingBottom: 12,
+        paddingLeft: 24,
+        paddingRight: 24
+    },
+    contentIcon: {
+        width: 36,
+        height: 36
+    },
+    contentText: {
+        color: '#91969d',
+        fontSize: 16
+    },
+    contentArrow: {
+        width: 24,
+        height: 24
+    },
+    arrowParent: {
+        flex: 1,
+        alignItems: 'flex-end'
     }
+
 });
 
 AppRegistry.registerComponent('QuHeart4', () => HomeConst);
